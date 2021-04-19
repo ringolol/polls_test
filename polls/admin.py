47fdfa7e0ac91d4548856deea9851962cdad2482
ringolol <createsite108@gmail.com>
2021-04-19 20:35:48 +0300

    extend admin models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(CompletedPoll)
+class CompletedPollAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(AsweredQuestion)
+class AsweredQuestionAdmin(admin.ModelAdmin):
+    pass
